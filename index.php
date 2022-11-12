b14cb46e234e27c11490208c707dbcfb890a4e40
pravinb2w <96853560+pravinb2w@users.noreply.github.com>
2022-11-12 13:49:28 +0530

    map

--- index.php
+++ index.php
@@ -1328,25 +1328,6 @@
 					<div class="col-lg-3 col-md-6">
 						<div class="single-footer-widget">
 							 
-                  <div class="map-overlay">
-                     <div class="container-fluid">
-                        <div class="row">
-                           <div class="main_maps text-center">
-                              <div class="col-sm-12 no-padding">
-                                 <div class="map_canvas_icon">
-                                    <i class="fa fa-map-marker" onClick="showmap()"></i>
-                                    <h2 onClick="showmap()">Find us on google map</h2>
-                                 </div>
-                                 <!-- <div id="map_canvas"></div> -->
-                                 <div style="width: 100%"><iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=450&amp;hl=en&amp;q=No:%20W5,%205th%20Main%20Road,%20Anna%20Nagar,%20Chennai%20-%20600040+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" loading="lazy"><a href="https://www.gps.ie/wearable-gps/">gps smartwatches</a></iframe></div>
-
-                                
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               
 						</div>
 					</div>
 				</div>
