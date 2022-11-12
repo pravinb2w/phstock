c1b8360509b46317f41d64c6eed0485d3d0ce222
pravinb2w <96853560+pravinb2w@users.noreply.github.com>
2022-11-12 15:21:03 +0530

    hyperlink

--- index.php
+++ index.php
@@ -395,7 +395,7 @@
 													<li>5GB Server Space</li>
 												</ul>
 					
-												<a href="#contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 											</div>
@@ -465,7 +465,7 @@
 													<li>10GB Server Space</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 					
@@ -537,7 +537,7 @@
 													<li> 20GB Server Space</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 											</div>
@@ -607,7 +607,7 @@
 													<li> Server from Customer</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 											</div>
@@ -681,7 +681,7 @@
 													<li>5GB Server Space</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 											</div>
@@ -751,7 +751,7 @@
 													<li>10GB Server Space</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 					
@@ -823,7 +823,7 @@
 													<li> 20GB Server Space</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 											</div>
@@ -892,7 +892,7 @@
 													<li> Server from Customer</li>
 												</ul>
 					
-												<a href="#Contact" class="default-btn">
+												<a href="#contact" class="default-btn saas-btn scroll-btn">
 													Read more
 												</a>
 											</div>
@@ -1366,7 +1366,7 @@
 
 					 <div class="col-lg-4 col-md-6">
 						<div class="single-footer-widget">
-						<h3>Find us on</h3>
+						<h3>Find us</h3>
 							 <div style="width: 100%"><iframe width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=450&amp;hl=en&amp;q=No:%20W5,%205th%20Main%20Road,%20Anna%20Nagar,%20Chennai%20-%20600040+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" loading="lazy"></iframe></div>
 						</div>
 					</div>
