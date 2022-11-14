0e02ee900df87e6beabce143181af89af4bba4d8
pravinb2w <96853560+pravinb2w@users.noreply.github.com>
2022-11-14 08:46:03 +0530

    mail text

--- function.php
+++ function.php
@@ -13,20 +13,20 @@ if( isset( $_POST ) && !empty( $_POST ) ) {
 	
     $body_message .= '<div>
                         <div>Dear '.$name.',</div>
-                        <div>Thanks for your enquiry the product. Our sales team will get back to shortly</div>                        
+                        <div>Thanks for your enquiry the product. Our sales team will get back to shortly <br></div>                        
                     </div>';
 					$body_message  .= '<div><table>';
-    $body_message  .= '<tr><b>Name:</b> '.$name.'</tr>';
-    $body_message  .= '<tr><b>Email:</b> '.$email.'</tr>';
-    $body_message  .= '<tr><b>Phone Number:</b> '.$name.'</tr>';
-	$body_message  .= '<tr><b>Package:</b> '.$msg_subject.'</tr>';
-    $body_message  .= '<tr><b>Company:</b> '.$product.'</tr>';
-    $body_message  .= '<tr><b>Message:</b> '.$message.'</tr>';
+    $body_message  .= '<tr><td><b>Name:</b><td><td> '.$name.'</td></tr>';
+    $body_message  .= '<tr><td><b>Email:</b><td><td> '.$email.'</td></tr>';
+    $body_message  .= '<tr><td><b>Phone Number:</b><td><td> '.$phone_number.'</td></tr>';
+	$body_message  .= '<tr><td><b>Package:</b><td><td> '.$product.'</td></tr>';
+    $body_message  .= '<tr><td><b>Company:</b><td><td> '.$msg_subject.'</td></tr>';
+    $body_message  .= '<tr><td><b>Message:</b><td><td> '.$message.'</td></tr>';
     $body_message  .= '</table></div>';
 	  $body_message .= '<div>
                         <br>
                         
-                        Regards
+                        Regards <br>
                         <b>Phoenix Technology Team</b>
                     </div>';
     try {
